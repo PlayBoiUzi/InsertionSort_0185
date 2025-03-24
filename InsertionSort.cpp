b8250f05b853f22bdadfa8d4f77236c5726bf5ea
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-03-24 19:34:20 +0700

    Mengimplementasikan algoritma insertion sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -32,3 +32,25 @@ cin >> arr[i]; // Menyinpan nilai data n kedalam array arr
 }
 }
 
+void insertion_sort()
+{ // Procedure insertionsort
+
+int temp ; // Membuat variable data temporer atau penyimpanan sementara
+int j, i; // Membuat variable j sebagai penanda
+
+for (i = 1; i < n; i++) // Step 1
+{
+    temp = arr[i]; // Step 2
+    j = i - 1; // Step 3
+
+    while (j >= 0 && arr[j] > temp) // Step 4
+    {
+        arr[j + 1] = arr[j]; // Step 4a
+        j--; // Step 4b
+    }
+
+    arr[j + 1] = temp; // Step 5
+}
+}
+
+
